1ffcdda4a703b08c4df877691782255206300de2
Matt Langlois <fletchto99@hotmail.com>
2015-02-25 01:22:12 -0500

    Source format + cleanup

--- src/RandomNumberArray.java
+++ src/RandomNumberArray.java
@@ -4,149 +4,148 @@ import java.util.HashMap;
 
 public class RandomNumberArray {
 
-	private final HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    private final HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-	public RandomNumberArray(final String numbers) {
-		this.parse(numbers.trim());
-	}
+    public RandomNumberArray(final String numbers) {
+        this.parse(numbers.trim());
+    }
 
-	public void add(final int number) {
-		if (this.map.containsKey(number)) {
-			this.map.put(number, this.map.get(number) + 1);
-		} else {
-			this.map.put(number, 1);
-		}
-	}
+    public void add(final int number) {
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+        }
+    }
 
-	public int evenCount() {
-		int count = 0;
-		for (final int i : this.map.keySet()) {
-			if (i % 2 == 0) {
-				count += this.map.get(i);
-			}
-		}
-		return count;
-	}
+    public int evenCount() {
+        int count = 0;
+        for (final int i : map.keySet()) {
+            if (i % 2 == 0) {
+                count += map.get(i);
+            }
+        }
+        return count;
+    }
 
-	public double findAverage() {
-		if (this.map.isEmpty()) {
-			return 0;
-		}
-		double total = 0;
-		double numbers = 0;
-		for (final int i : this.map.keySet()) {
-			total += this.map.get(i) * i;
-			numbers += this.map.get(i);
-		}
-		return total / numbers;
-	}
+    public double findAverage() {
+        if (map.isEmpty()) {
+            return 0;
+        }
+        double total = 0;
+        double numbers = 0;
+        for (final int i : map.keySet()) {
+            total += map.get(i) * i;
+            numbers += map.get(i);
+        }
+        return total / numbers;
+    }
 
-	public int[] findMode() {
-		if (this.map.isEmpty()) {
-			return new int[] { 0 };
-		}
-		final int max = Collections.max(this.map.values());
-		final ArrayList<Integer> modes = new ArrayList<Integer>();
-		for (final int i : this.map.keySet()) {
-			if (this.map.get(i) == max) {
-				modes.add(i);
-			}
-		}
-		final int[] result = new int[modes.size() + 1];
-		result[0] = max;
-		for (int i = 0; i < modes.size(); i++) {
-			result[i + 1] = modes.get(i);
-		}
-		return result;
-	}
+    public int[] findMode() {
+        if (map.isEmpty()) {
+            return new int[] { 0 };
+        }
+        final int max = Collections.max(map.values());
+        final ArrayList<Integer> modes = new ArrayList<Integer>();
+        for (final int i : map.keySet()) {
+            if (map.get(i) == max) {
+                modes.add(i);
+            }
+        }
+        final int[] result = new int[modes.size() + 1];
+        result[0] = max;
+        for (int i = 0; i < modes.size(); i++) {
+            result[i + 1] = modes.get(i);
+        }
+        return result;
+    }
 
-	public int getAmount(final int number) {
-		return this.map.containsKey(number) ? this.map.get(number) : -1;
-	}
+    public int getAmount(final int number) {
+        return map.containsKey(number) ? map.get(number) : -1;
+    }
 
-	public ArrayList<Integer> getNumberArray() {
-		final ArrayList<Integer> sorted = new ArrayList<Integer>(
-				this.map.keySet());
-		Collections.sort(sorted);
-		return sorted;
-	}
+    public ArrayList<Integer> getNumberArray() {
+        final ArrayList<Integer> sorted = new ArrayList<Integer>(map.keySet());
+        Collections.sort(sorted);
+        return sorted;
+    }
 
-	private boolean isPrime(final int n) {
-		if (n % 2 == 0) {
-			return false;
-		}
-		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean isPrime(final int n) {
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public int oddCount() {
-		int count = 0;
-		for (final int i : this.map.keySet()) {
-			if (i % 2 == 1) {
-				count += this.map.get(i);
-			}
-		}
-		return count;
-	}
+    public int oddCount() {
+        int count = 0;
+        for (final int i : map.keySet()) {
+            if (i % 2 == 1) {
+                count += map.get(i);
+            }
+        }
+        return count;
+    }
 
-	private void parse(final String numbers) {
-		final String replaced = numbers.replaceAll("[^0-9\\s]", "");
-		final String[] numArrayString = replaced.split("\\s+");
-		for (final String s : numArrayString) {
-			Integer num;
-			try {
-				num = Integer.parseInt(s);
-			} catch (final Exception e) {
-				continue;
-			}
-			if (this.map.containsKey(num)) {
-				this.map.put(num, this.map.get(num) + 1);
-			} else {
-				this.map.put(num, 1);
-			}
-		}
-	}
+    private void parse(final String numbers) {
+        final String replaced = numbers.replaceAll("[^0-9\\s]", "");
+        final String[] numArrayString = replaced.split("\\s+");
+        for (final String s : numArrayString) {
+            Integer num;
+            try {
+                num = Integer.parseInt(s);
+            } catch (final Exception e) {
+                continue;
+            }
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+    }
 
-	public int primeCount() {
-		int count = 0;
-		for (final int i : this.map.keySet()) {
-			if (this.isPrime(i)) {
-				count += this.map.get(i);
-			}
-		}
-		return count;
-	}
+    public int primeCount() {
+        int count = 0;
+        for (final int i : map.keySet()) {
+            if (this.isPrime(i)) {
+                count += map.get(i);
+            }
+        }
+        return count;
+    }
 
-	public int range() {
-		return Collections.max(this.map.keySet())
-				- Collections.min(this.map.keySet()) + 1;
-	}
+    public int range() {
+        return Collections.max(map.keySet()) - Collections.min(map.keySet())
+                + 1;
+    }
 
-	public void remove(final int number) {
-		if (this.map.containsKey(number)) {
-			if (this.map.get(number) == 1) {
-				this.map.remove(number);
-			} else {
-				this.map.put(number, this.map.get(number) - 1);
-			}
-		}
-	}
+    public void remove(final int number) {
+        if (map.containsKey(number)) {
+            if (map.get(number) == 1) {
+                map.remove(number);
+            } else {
+                map.put(number, map.get(number) - 1);
+            }
+        }
+    }
 
-	public void removeAll(final double number) {
-		if (this.map.containsKey(number)) {
-			this.map.remove(number);
-		}
-	}
+    public void removeAll(final double number) {
+        if (map.containsKey(number)) {
+            map.remove(number);
+        }
+    }
 
-	public int total() {
-		int total = 0;
-		for (final int i : this.map.values()) {
-			total += i;
-		}
-		return total;
-	}
+    public int total() {
+        int total = 0;
+        for (final int i : map.values()) {
+            total += i;
+        }
+        return total;
+    }
 }
--- src/Randomness.java
+++ src/Randomness.java
@@ -16,175 +16,177 @@ import javax.swing.table.DefaultTableModel;
 
 public class Randomness extends JApplet {
 
-	private static final long serialVersionUID = -1737807640424560213L;
-	private JScrollPane scrollPane1;
-	private JScrollPane scrollPane2;
-	private JScrollPane scrollPane3;
-	private JTextPane input;
-	private JTable table;
-	private JTextArea information;
-	private RandomNumberArray numbers = null;
+    private static final long serialVersionUID = -1737807640424560213L;
+    private JScrollPane scrollPane1;
+    private JScrollPane scrollPane2;
+    private JScrollPane scrollPane3;
+    private JTextPane input;
+    private JTable table;
+    private JTextArea information;
+    private RandomNumberArray numbers = null;
 
-	private String formatMode() {
-		String mode = "The number"
-				+ (this.numbers.findMode().length > 2 ? "s " : "") + " that "
-				+ (this.numbers.findMode().length > 2 ? "have" : "has")
-				+ " the most "
-				+ (this.numbers.findMode().length > 2 ? "are " : "is ");
-		for (int i = 1; i < this.numbers.findMode().length; i++) {
-			mode += String.valueOf(this.numbers.findMode()[i]);
-			if (i < this.numbers.findMode().length - 1) {
-				mode += ", ";
-			}
-		}
-		mode += " with "
-				+ (this.numbers.findMode().length > 2 ? "each" : "it")
-				+ " being selected "
-				+ String.valueOf(this.numbers.findMode()[0])
-				+ " time"
-				+ (this.numbers.findMode()[0] > 1 ? "s " : "")
-				+ " which is worth "
-				+ new DecimalFormat("#,##0.0").format(this.percentage(
-						this.numbers.findMode()[0]
-								* (this.numbers.findMode().length - 1),
-						this.numbers.total())) + "%";
-		return mode;
-	}
+    private String formatMode() {
+        String mode = "The number"
+                + (numbers.findMode().length > 2 ? "s " : "") + " that "
+                + (numbers.findMode().length > 2 ? "have" : "has")
+                + " the most "
+                + (numbers.findMode().length > 2 ? "are " : "is ");
+        for (int i = 1; i < numbers.findMode().length; i++) {
+            mode += String.valueOf(numbers.findMode()[i]);
+            if (i < numbers.findMode().length - 1) {
+                mode += ", ";
+            }
+        }
+        mode += " with "
+                + (numbers.findMode().length > 2 ? "each" : "it")
+                + " being selected "
+                + String.valueOf(numbers.findMode()[0])
+                + " time"
+                + (numbers.findMode()[0] > 1 ? "s " : "")
+                + " which is worth "
+                + new DecimalFormat("#,##0.0")
+                        .format(this.percentage(numbers.findMode()[0]
+                                * (numbers.findMode().length - 1),
+                                numbers.total())) + "%";
+        return mode;
+    }
 
-	@Override
-	public void init() {
-		SwingUtilities
-				.invokeLater(() -> {
-					try {
-						UIManager.setLookAndFeel(UIManager
-								.getSystemLookAndFeelClassName());
-					} catch (final Exception e) {
-						e.printStackTrace();
-					}
-					JFrame.setDefaultLookAndFeelDecorated(true);
-					Randomness.this.setSize(720, 770);
+    @Override
+    public void init() {
+        SwingUtilities
+                .invokeLater(() -> {
+                    try {
+                        UIManager.setLookAndFeel(UIManager
+                                .getSystemLookAndFeelClassName());
+                    } catch (final Exception e) {
+                        e.printStackTrace();
+                    }
+                    JFrame.setDefaultLookAndFeelDecorated(true);
+                    Randomness.this.setSize(720, 770);
 
-					Randomness.this.scrollPane1 = new JScrollPane();
-					Randomness.this.scrollPane2 = new JScrollPane();
-					Randomness.this.scrollPane3 = new JScrollPane();
-					Randomness.this.input = new JTextPane();
-					Randomness.this.table = new JTable();
-					Randomness.this.information = new JTextArea();
+                    Randomness.this.scrollPane1 = new JScrollPane();
+                    Randomness.this.scrollPane2 = new JScrollPane();
+                    Randomness.this.scrollPane3 = new JScrollPane();
+                    Randomness.this.input = new JTextPane();
+                    Randomness.this.table = new JTable();
+                    Randomness.this.information = new JTextArea();
 
-					final Container contentPane = Randomness.this
-							.getContentPane();
-					contentPane.setLayout(null);
+                    final Container contentPane = Randomness.this
+                            .getContentPane();
+                    contentPane.setLayout(null);
 
-					Randomness.this.input.addKeyListener(new KeyListener() {
+                    Randomness.this.input.addKeyListener(new KeyListener() {
 
-						@Override
-						public void keyPressed(final KeyEvent arg0) {
-						}
+                        @Override
+                        public void keyPressed(final KeyEvent arg0) {
+                        }
 
-						@Override
-						public void keyReleased(final KeyEvent arg0) {
-							if (Randomness.this.input.getText().length() > 0) {
-								Randomness.this.numbers = new RandomNumberArray(
-										Randomness.this.input.getText());
-							} else {
-								Randomness.this.numbers = null;
-							}
-							Randomness.this.populateTable();
-							Randomness.this.updateInformation();
-						}
+                        @Override
+                        public void keyReleased(final KeyEvent arg0) {
+                            if (input.getText().length() > 0) {
+                                numbers = new RandomNumberArray(input.getText());
+                            } else {
+                                numbers = null;
+                            }
+                            Randomness.this.populateTable();
+                            Randomness.this.updateInformation();
+                        }
 
-						@Override
-						public void keyTyped(final KeyEvent arg0) {
-						}
+                        @Override
+                        public void keyTyped(final KeyEvent arg0) {
+                        }
 
-					});
-					Randomness.this.scrollPane1
-							.setViewportView(Randomness.this.input);
-					contentPane.add(Randomness.this.scrollPane1);
-					Randomness.this.scrollPane1.setBounds(15, 20, 675, 185);
+                    });
+                    Randomness.this.scrollPane1
+                            .setViewportView(Randomness.this.input);
+                    contentPane.add(Randomness.this.scrollPane1);
+                    Randomness.this.scrollPane1.setBounds(15, 20, 675, 185);
 
-					((DefaultTableModel) Randomness.this.table.getModel())
-							.addColumn("Number");
-					((DefaultTableModel) Randomness.this.table.getModel())
-							.addColumn("Amount");
-					Randomness.this.scrollPane2
-							.setViewportView(Randomness.this.table);
-					contentPane.add(Randomness.this.scrollPane2);
-					Randomness.this.scrollPane2.setBounds(15, 210, 675, 285);
+                    ((DefaultTableModel) Randomness.this.table.getModel())
+                            .addColumn("Number");
+                    ((DefaultTableModel) Randomness.this.table.getModel())
+                            .addColumn("Amount");
+                    Randomness.this.scrollPane2
+                            .setViewportView(Randomness.this.table);
+                    contentPane.add(Randomness.this.scrollPane2);
+                    Randomness.this.scrollPane2.setBounds(15, 210, 675, 285);
 
-					Randomness.this.information.setLineWrap(true);
-					Randomness.this.information.setWrapStyleWord(true);
-					Randomness.this.information.setEditable(false);
-					Randomness.this.information.setFont(new Font("Tahoma", 0,
-							20));
-					Randomness.this.information
-							.setText("Please insert some values in the text box above. To seprate values use a single space.");
-					Randomness.this.scrollPane3
-							.setViewportView(Randomness.this.information);
-					contentPane.add(Randomness.this.scrollPane3);
-					Randomness.this.scrollPane3.setBounds(15, 505, 675, 220);
+                    Randomness.this.information.setLineWrap(true);
+                    Randomness.this.information.setWrapStyleWord(true);
+                    Randomness.this.information.setEditable(false);
+                    Randomness.this.information.setFont(new Font("Tahoma", 0,
+                            20));
+                    Randomness.this.information
+                            .setText("Please insert some values in the text box above. To seprate values use a single space.");
+                    Randomness.this.scrollPane3
+                            .setViewportView(Randomness.this.information);
+                    contentPane.add(Randomness.this.scrollPane3);
+                    Randomness.this.scrollPane3.setBounds(15, 505, 675, 220);
 
-				});
-	}
+                });
+    }
 
-	private double percentage(final double number, final double total) {
-		return number / total * 100;
-	}
+    private double percentage(final double number, final double total) {
+        return number / total * 100;
+    }
 
-	public void populateTable() {
-		final DefaultTableModel model = (DefaultTableModel) this.table
-				.getModel();
-		for (int i = model.getRowCount() - 1; i >= 0; i--) {
-			model.removeRow(i);
-		}
-		if (this.numbers != null) {
-			for (final int i : this.numbers.getNumberArray()) {
-				model.addRow(new Object[] { String.valueOf(i),
-						String.valueOf(this.numbers.getAmount(i)) });
-			}
-		}
-	}
+    public void populateTable() {
+        final DefaultTableModel model = (DefaultTableModel) table.getModel();
+        for (int i = model.getRowCount() - 1; i >= 0; i--) {
+            model.removeRow(i);
+        }
+        if (numbers != null) {
+            for (final int i : numbers.getNumberArray()) {
+                model.addRow(new Object[] { String.valueOf(i),
+                        String.valueOf(numbers.getAmount(i)) });
+            }
+        }
+    }
 
-	private void updateInformation() {
-		if (this.numbers != null) {
-			this.information.setText(this.formatMode()
-					+ ". The average number is "
-					+ new DecimalFormat("#,##0.0").format(this.numbers
-							.findAverage())
-					+ ". The ratio of evens to odds is "
-					+ this.numbers.evenCount()
-					+ ":"
-					+ this.numbers.oddCount()
-					+ " where even numbers make up "
-					+ new DecimalFormat("#,##0.0").format(this.percentage(
-							this.numbers.evenCount(), this.numbers.total()))
-					+ "% and odds make up "
-					+ new DecimalFormat("#,##0.0").format(this.percentage(
-							this.numbers.oddCount(), this.numbers.total()))
-					+ "%. The ratio of prime numbers to non-prime numbers is "
-					+ this.numbers.primeCount()
-					+ ":"
-					+ (this.numbers.total() - this.numbers.primeCount())
-					+ " where prime numbers make up "
-					+ new DecimalFormat("#,##0.0").format(this.percentage(
-							this.numbers.primeCount(), this.numbers.total()))
-					+ "% and non-prime numbers make up "
-					+ new DecimalFormat("#,##0.0").format(this.percentage(
-							(this.numbers.total() - this.numbers.primeCount()),
-							this.numbers.total()))
-					+ "%. The total amount of numbers tested is "
-					+ this.numbers.total()
-					+ " which has a range of "
-					+ this.numbers.range()
-					+ " numbers with "
-					+ this.numbers.getNumberArray().get(0)
-					+ " being the lowest number and "
-					+ this.numbers.getNumberArray().get(
-							this.numbers.getNumberArray().size() - 1)
-					+ " being the highest number.");
-		} else {
-			this.information
-			.setText("Please insert some values in the text box above. To seprate values use a single space.");
-		}
-	}
+    private void updateInformation() {
+        if (numbers != null) {
+            information
+                    .setText(this.formatMode()
+                            + ". The average number is "
+                            + new DecimalFormat("#,##0.0").format(numbers
+                                    .findAverage())
+                            + ". The ratio of evens to odds is "
+                            + numbers.evenCount()
+                            + ":"
+                            + numbers.oddCount()
+                            + " where even numbers make up "
+                            + new DecimalFormat("#,##0.0").format(this
+                                    .percentage(numbers.evenCount(),
+                                            numbers.total()))
+                            + "% and odds make up "
+                            + new DecimalFormat("#,##0.0").format(this
+                                    .percentage(numbers.oddCount(),
+                                            numbers.total()))
+                            + "%. The ratio of prime numbers to non-prime numbers is "
+                            + numbers.primeCount()
+                            + ":"
+                            + (numbers.total() - numbers.primeCount())
+                            + " where prime numbers make up "
+                            + new DecimalFormat("#,##0.0").format(this
+                                    .percentage(numbers.primeCount(),
+                                            numbers.total()))
+                            + "% and non-prime numbers make up "
+                            + new DecimalFormat("#,##0.0").format(this
+                                    .percentage((numbers.total() - numbers
+                                            .primeCount()), numbers.total()))
+                            + "%. The total amount of numbers tested is "
+                            + numbers.total()
+                            + " which has a range of "
+                            + numbers.range()
+                            + " numbers with "
+                            + numbers.getNumberArray().get(0)
+                            + " being the lowest number and "
+                            + numbers.getNumberArray().get(
+                                    numbers.getNumberArray().size() - 1)
+                            + " being the highest number.");
+        } else {
+            information
+                    .setText("Please insert some values in the text box above. To seprate values use a single space.");
+        }
+    }
 }
